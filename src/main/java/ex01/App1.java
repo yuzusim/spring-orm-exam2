ebddab4368db00c630784283a4ea043fe2d07edd
yuzusim1~ <yuzusim@gmail.com>
2024-02-15 19:16:13 +0900

    orm  ex01 연습

--- src/main/java/ex01/App1.java
+++ src/main/java/ex01/App1.java
@@ -1,11 +1,29 @@
 package ex01;
 
+import ex01.model.User;
+
 public class App1 {
     public static void main(String[] args) {
         // 1. DB에서 가져온 데이터 - Flat(평평) 하게 가져올 수 밖에 없다
         DBData1 dbData = new DBData1(1, "제목1", "내용1", 3, "love", "love@nate.com");
 
         // 2. dbData를 ViewData에 옮기시오 - ORM
+        ViewData1 viewData = new ViewData1();
+        viewData.setBoardId(dbData.getBoardId());
+        viewData.setTitle(dbData.getTitle());
+        viewData.setContent(dbData.getContent());
+        viewData.setContent(dbData.getContent());
+
+
+        User user = new User();
+        user.setUserId(dbData.getUserId());
+        user.setUsername(dbData.getUsername());
+        user.setEmail(dbData.getEmail());
+
+        viewData.setUser(user);
+
+        System.out.println(viewData);
+
 
     }
 }
--- src/main/java/ex02/App2.java
+++ src/main/java/ex02/App2.java
@@ -1,13 +1,19 @@
 package ex02;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class App2 {
     public static void main(String[] args) {
         // 1. DB에서 가져온 데이터 - Flat(평평) 하게 가져올 수 밖에 없다
         DBData2 dbData1 = new DBData2(1, "제목1", "내용1", 1, "댓글1");
         DBData2 dbData2 = new DBData2(1, "제목1", "내용1", 2, "댓글2");
-        DBData2 dbData4 = new DBData2(2, "제목2", "내용2", 3, "댓글3");
+        DBData2 dbData3 = new DBData2(1, "제목1", "내용1", 3, "댓글3");
 
-        // 2. dbData를 ViewData에 옮기시오 - ORM
-        
+        List<DBData2> dbList = Arrays.asList(dbData1, dbData2, dbData3);
+
+        // 2. dbList(컬렉션을) ViewData2(오브젝트)에 옮기시오 - ORM
+
     }
 }
